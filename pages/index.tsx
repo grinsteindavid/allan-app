2b3b6dcd2e24857a6ed90d5a54d2b3eb6606b775
david <davidmirandagrinstein@gmail.com>
2021-04-28 16:32:20 -0400

    defaultTheme and jsonplaceholder example

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,8 @@ import Image from 'next/image'
 
 
 
-export default function Home() {
+export default function HomePage() {
+
 	return (
 		<Container>
 			<Head>
--- themes/default.ts
+++ themes/default.ts
@@ -1,3 +1,7 @@
-export default {
-
+const defaultTheme = {
+    primaryColor: 'blue',
 }
+
+export type defaultThemeType = typeof defaultTheme
+
+export default defaultTheme
